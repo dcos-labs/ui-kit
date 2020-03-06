72ed88c0918941f82c3d95a2e65eb0bca9881fc4
Mike Perrotti <mperrotti573@gmail.com>
2020-03-06 14:09:33 -0500

    chore: pairing session WIP

--- packages/link/components/ResetLink.tsx
+++ packages/link/components/ResetLink.tsx
@@ -3,25 +3,62 @@ import { cx } from "emotion";
 import { LinkProps } from "../types";
 import { linkReset } from "../../shared/styles/styleUtils/resets/linkReset";
 
-const ResetLink: React.StatelessComponent<LinkProps> = ({
+const TestComponent: React.StatelessComponent<{testProp: string;}> = (props) => (<div {...props} />);
+// type Foo<C extends React.ElementType> = React.ComponentProps<C>;
+
+{/*  */}
+
+function ResetLink<P extends {href: string | undefined; target: string | undefined; children: React.ReactNode;}>({
   children,
   className,
   href,
   openInNewTab,
   target,
   url,
+  src,
+  testProp,
+  component: NewComponent,
+  testinggg,
   ...other
-}) => (
-  <a
+}: LinkProps
+  & {
+      component: React.SFC<P>;
+      componentProps: P
+    }
+): React.ReactElement {
+  return (
+  <NewComponent
     href={href || url}
     target={!target && openInNewTab ? "_blank" : target}
-    className={cx(linkReset, className)}
+    // className={cx(linkReset, className)}
     // https://web.dev/external-anchors-use-rel-noopener/
-    rel={target === "_blank" || openInNewTab ? "noopener" : undefined}
-    {...other}
+    // rel={target === "_blank" || openInNewTab ? "noopener" : undefined}
+    // {...other}
   >
     {children}
-  </a>
-);
+  </NewComponent>
+)};
+// const ResetLink = ({
+//   children,
+//   className,
+//   href,
+//   openInNewTab,
+//   target,
+//   url,
+//   testProp,
+//   component,
+//   ...other
+// }) => (
+//   <a
+//     href={href || url}
+//     target={!target && openInNewTab ? "_blank" : target}
+//     className={cx(linkReset, className)}
+//     // https://web.dev/external-anchors-use-rel-noopener/
+//     rel={target === "_blank" || openInNewTab ? "noopener" : undefined}
+//     {...other}
+//   >
+//     {children}
+//   </a>
+// );
 
 export default ResetLink;
